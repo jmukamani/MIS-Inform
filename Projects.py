1ecb48c8f073bd31ce2f5687d45ca7b29180994b
jmukamani <j.mukamani@alustudent.com>
2023-04-02 22:11:53 +0200

    Project finalized

--- Projects.py
+++ Projects.py
@@ -6,3 +6,9 @@ project_description = "Renovating Amahoro stadium to align with international st
 duration_months = 12
 estimated_completion_date = datetime.date.today() + datetime.timedelta(days=duration_months*30)
 project_status = "Ongoing"
+
+print("Name:", project_name)
+print("Location:", project_location)
+print("Description:", project_description)
+print("Estimated Completion Date:", estimated_completion_date.strftime("%d %B %Y"))
+print("Status:", project_status)
