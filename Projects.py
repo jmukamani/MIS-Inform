8c052a96753cfd2bc5782366bd2d9ceec818ec87
jmukamani <j.mukamani@alustudent.com>
2023-04-02 22:09:15 +0200

    Projects

---
+++ Projects.py
@@ -0,0 +1,8 @@
+import datetime
+
+project_name = "Amahoro Stadium Refurbishment"
+project_location = "Kigali City, Kigali Province"
+project_description = "Renovating Amahoro stadium to align with international stadium standards. The objective is to make Kigali a sporting destination and promote tourism in Rwanda."
+duration_months = 12
+estimated_completion_date = datetime.date.today() + datetime.timedelta(days=duration_months*30)
+project_status = "Ongoing"
